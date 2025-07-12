5b7950dca5e664996ee20c6fa05d2e0203ff11e1
Gustavo Perdomo <gperdomor@gmail.com>
2025-07-12 13:56:37 -0400

    feat: added middlewares (#2)

--- main.go
+++ main.go
@@ -3,6 +3,10 @@ package main
 import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/log"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/healthcheck"
+	"github.com/gofiber/fiber/v2/middleware/helmet"
+	"github.com/gofiber/fiber/v2/middleware/recover"
 )
 
 func main() {
@@ -17,6 +21,12 @@ func main() {
 func Setup() *fiber.App {
 	// Initialize a new app
 	app := fiber.New()
+
+	// Middlewares
+	app.Use(recover.New())
+	app.Use(helmet.New())
+	app.Use(cors.New())
+	app.Use(healthcheck.New())
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.Status(fiber.StatusOK).JSON(fiber.Map{
